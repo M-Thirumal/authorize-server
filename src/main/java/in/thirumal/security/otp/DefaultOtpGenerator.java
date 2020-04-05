518cc7bef3f35d48f6c88b7793da59cfefc05d9f
Thirumal <m.thirumal@hotmail.com>
2020-04-05 19:53:34 +0530

    F

--- src/main/java/in/thirumal/security/otp/DefaultOtpGenerator.java
+++ src/main/java/in/thirumal/security/otp/DefaultOtpGenerator.java
@@ -17,15 +17,13 @@ public class DefaultOtpGenerator implements OtpGenerator {
 	
 	private static final char[] CHARS = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 			'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3',
-			'4', '5', '6', '7', '8', '9'};
-	private Random rand;
-	
+			'4', '5', '6', '7', '8', '9'};	
 	/* (non-Javadoc)
 	 * @see com.enkindle.security.otp.OtpGenerator#generateToken()
 	 */
 	@Override
 	public String generateToken(int length) {
-		rand = new SecureRandom();
+		Random rand = new SecureRandom();
 		StringBuilder sb = new StringBuilder(length);
 		for (int i = 0; i < length; i++) {
 			sb.append(CHARS[rand.nextInt(CHARS.length)]);
